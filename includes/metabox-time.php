c7fdaf1dd572f20e9ee9753d6004b4be85ee0c90
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2022-12-01 08:47:00 -0500

    Correcion input time

--- includes/metabox-time.php
+++ includes/metabox-time.php
@@ -7,7 +7,7 @@ class MetaboxTime{
 
 	public function __construct(){
 		add_action( 'add_meta_boxes', [$this, 'dcms_add_metabox_time'] );
-		add_action( 'save_post_stm-courses', [$this, 'dcms_save_metabox_content'], 20, 2 );
+		add_action( 'save_post_stm-courses', [$this, 'dcms_save_metabox_content'], 30, 2 ); // Priority 30
 	}
 
 	public function dcms_add_metabox_time(){
@@ -22,7 +22,7 @@ class MetaboxTime{
 
 	public function dcms_add_metabox_content( $post ){
 		$start_time = get_post_meta( $post->ID, DCMS_NOTIF_COURSE_TIME, true );
-
+		$start_time = date('H:i', strtotime($start_time));
 		?>
 		<label for="course-time" >Hora de inicio</label>
 		<input id="course-time" name="course-time" type="time" value="<?= $start_time ?>" >
@@ -32,7 +32,8 @@ class MetaboxTime{
 	public function dcms_save_metabox_content( $post_id, $post ){
 		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
 
-		$start_time = $_POST['link-product'];
+		$meta_date = get_post_meta( $post_id, DCM_COURSE_DATE, true );
+		$start_time = $meta_date . ' ' . $_POST['course-time'];
 		update_post_meta( $post_id, DCMS_NOTIF_COURSE_TIME, $start_time );
 	}
 }
